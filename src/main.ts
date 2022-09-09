0b70055eda64dba7528214986d03f5dabea460ac
Pedro <pedro@cff.org.br>
2022-09-08 22:37:21 -0300

    colocando public

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000, function () {
-    console.log('Express server listening on port %d', this.address().port);
-  });
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
