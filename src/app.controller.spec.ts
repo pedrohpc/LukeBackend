a3adf7684fae08413166c5dbc4595f9ea1fc9c6b
Pedro <pedro@cff.org.br>
2022-09-08 21:04:49 -0300

    colocando public

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getLukePreso()).toBe({ statusPreso: Boolean });
+      expect(appController.getLukePreso()).toBe({ statusPreso: false });
     });
   });
 });
